0c47a145b48bb616dce8011fe267c47bc0935c4b
Yanghai <yanghai.maxwell.cong@outlook.com>
2018-07-24 10:30:27 +0800

    small changes

--- ggplot2.r
+++ ggplot2.r
@@ -2,8 +2,12 @@
 library(ggplot2)
 attach(iris)
 p <- ggplot(data=iris,aes(x = Sepal.Length,y = Sepal.Width))
-p + geom_point(aes(colour = Species)) + stat_smooth() + 
-labs(title = "Iris of Sepal.length \n According to the Sepal.Width") +
-theme_classic() + theme_bw() +annotate("text",x=7,y=4,parse = T,label = "x[1]==x[2]",size=6, family="serif",fontface="italic", colour="darkred")
+p + 
+        geom_point(aes(colour = Species)) + 
+        stat_smooth() + 
+        labs(title = "Iris of Sepal.length \n According to the Sepal.Width") +
+        theme_classic() + 
+        theme_bw() +
+        annotate("text",x=7,y=4,parse = T,label = "x[1]==x[2]",size=6, family="serif",fontface="italic", colour="darkred")
 
 
