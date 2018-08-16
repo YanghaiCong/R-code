91e3091c226a33390f85f89017853f3c107ba470
Yanghai Cong <32158830+YanghaiCong@users.noreply.github.com>
2018-08-16 23:50:22 +0800

    yes

--- R_programming/the_art_of_r_programming.r
+++ R_programming/the_art_of_r_programming.r
@@ -2,92 +2,71 @@
 
 i<-1
 while (i<=10) {
-      i<-i+4
+    i<-i+4
 }
 print(i)
+
 
 i<-1
 while (TRUE) {
-      i<-i+4
-      if (i>10) break
+    i<-i+4
+    if (i>10) break
 }
 print(i)
 
+# repeat
 i<-1
 repeat {
-      i<-i+4
-      if (i>10) break
+    i<-i+4
+    if (i>10) break
 }
 print(i)
 
+# choose 5 people for committe A from 20 people
+# see which happens:
+## if 1&2, then add one to the total, start over
+## if 1|2, then start over
+## else, then
+### choose 4 people from the remaining 15 people
+### see which happens:
+#### if 1&2, then add one to the total, start over
+#### if 1|2, then start over
+#### else, then
+##### choose 3 people from the remaining 11 people
+##### see which happens:
+###### if 1&2, then add one to the total, start over
+###### if 1|2, then start over
+###### else, start over
 
-
-sim <- function(nreps) {
-      commdata <- list()
-      commdata$countabsamecomm <- 0 
-      for (rep in 1:nreps) { 
-            commdata$whosleft <- 1:20
-            commdata$numabchosen <- 0
-            
-            commdata <- choosecomm(commdata,5)
-
-            if (commdata$numabchosen > 0) next 
- 
-            commdata <- choosecomm(commdata,4) 
-            if (commdata$numabchosen > 0) next 
-
-            commdata <- choosecomm(commdata,3)
-      } 
-      print(commdata$countabsamecomm/nreps)
-      print(commdata)
+sim<-function(nreps){
+    committeeData<-list()
+    committeeData$countABSameCommittee<-0
+    
+    for(i in 1:nreps){
+        committeeData$whoIsLeft<-1:20
+        committeeData$numberOfAB<-0 
+        
+        committeeData<-formCommittee(committeeData,5)
+        if(committeeData$numberOfAB>0) next
+        
+        committeeData<-formCommittee(committeeData,4)
+        if(committeeData$numberOfAB>0) next
+        
+        committeeData<-formCommittee(committeeData,3)
+    }
+    print(committeeData$countABSameCommittee/nreps)
 }
 
 
-choosecomm <- function(comdat,comsize) {
-
-      committee <- sample(comdat$whosleft,comsize) 
-
-      comdat$numabchosen <- length(intersect(1:2,committee)) 
-      if (comdat$numabchosen == 2) 
-            comdat$countabsamecomm <- comdat$countabsamecomm + 1 
-
-      comdat$whosleft <- setdiff(comdat$whosleft,committee) 
-      return(comdat)
+formCommittee<-function(committeeData,numberOfCommitteeMembersToChoose){
+    chosen<-sample(committeeData$whoIsLeft,numberOfCommitteeMembersToChoose)
+    committeeData$numberOfAB<-length(intersect(1:2,chosen))
+    if(committeeData$numberOfAB==2)
+        committeeData$countABSameCommittee<-committeeData$countABSameCommittee+1
+    committeeData$whoIsLeft<-setdiff(committeeData$whoIsLeft,chosen)
+    return(committeeData)
 }
 
-
-
-
-# not finished
-my_sim<-function(nrep){
-      
-      mySimList<-list()
-      mySimList$sameCount<-0
-      
-      for (i in 1:nrep){
-            mySimList$left<-1:20
-            mySimList$flag<-0
-            
-            mySimList<-chsCom(mySimList,5)
-            if(mySimList$flag>0) next
-
-            mySimList<-chsCom(mySimList,4)
-            if(mySimList$flag>0) next
-            
-            mySimList<-chsCom(mySimList,3)
-      }
-      return(mySimList$sameCount/nrep)
-}
-
-chsCom<-function(mySimList,num){
-      selected<-sample(mySimList$left,size = num)
-      if(length(intersect(x = 1:2, y = selected)) == 0){
-            mySimList$sameCount<-mySimList$sameCount+1
-            mySimList$left<-setdiff(mySimList$left,selected)
-            mySimList$flag<-1
-      }
-      return(mySimList)
-}
 
 
 
