56185ed32b936e375abfac5ba6cbc557abf4fee6
Yanghai Cong <32158830+YanghaiCong@users.noreply.github.com>
2018-08-23 14:14:57 +0800

    Update to_remember.R

--- to_remember.R
+++ to_remember.R
@@ -6,3 +6,4 @@ install.packages("readxl")
 
 # for error like "Error in plot.new() : figure margins too large", change the following parameter
 par(mar=c(1,1,1,1))
+# default par(mar=c(5.1,4.1,4.1,2.1))
